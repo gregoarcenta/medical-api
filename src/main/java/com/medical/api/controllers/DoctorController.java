3da789e43821c5fb3e6f6b8c71ebe5d48533cead
Gregory Arcentales <gregoarcenta@gmail.com>
2026-01-13 22:05:52 -0500

    Add database migration for Doctor table, Flyway setup, and transactional support in createDoctor endpoint

--- src/main/java/com/medical/api/controllers/DoctorController.java
+++ src/main/java/com/medical/api/controllers/DoctorController.java
@@ -3,6 +3,7 @@ package com.medical.api.controllers;
 import com.medical.api.dto.DoctorRequest;
 import com.medical.api.models.Doctor;
 import com.medical.api.repository.DoctorRepository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,17 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/doctors")
 class DoctorController {
-
-
     private final DoctorRepository doctorRepository;
 
     DoctorController(DoctorRepository doctorRepository) {
         this.doctorRepository = doctorRepository;
     }
 
+    @Transactional
     @PostMapping
     public Doctor createDoctor(@RequestBody DoctorRequest doctor) {
         return doctorRepository.save(new Doctor(doctor));
     }
-
 }
